aa0f2be5f82e9556c2a174ab8b9aeabdeace93b3
spark <84654021+spark-s-s@users.noreply.github.com>
2023-06-16 11:05:28 +0900

    save

---
+++ ABC/224/c.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using str = string;
+using ll = long long;
+using ld = long double;
+using vb = vector<bool>;
+using vvb = vector<vector<bool>>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<long long>;
+using vvl = vector<vector<long long>>;
+#define REP(i, n) for (int i = 0; i < int(n); i++)
+#define RREP(i, n) for (int i = int(n) - 1; i >= 0; i--)
+#define FOR(i, f, t) for (int i = int(f); i <= int(t); i++)
+#define RFOR(i, f, t) for (int i = int(f); i >= int(t); i--)
+#define ALL(vec) (vec).begin(), (vec).end()
+#define SORT(vec) sort((vec).begin(), (vec).end())
+#define MAX(vec) *max_element((vec).begin(), (vec).end())
+#define MIN(vec) *min_element((vec).begin(), (vec).end())
+#define PB(x) push_back(x);
+#define YES(ans) if (ans) cout << "YES" << endl; else cout << "NO" << endl;
+#define Yes(ans) if (ans) cout << "Yes" << endl; else cout << "No" << endl;
+#define yes(ans) if (ans) cout << "yes" << endl; else cout << "no" << endl;
+const double PI = 3.141592653589793;
+
+int main() {
+  int n; cin >> n;
+  vvi data(n, vi(2));
+  REP(i, n) cin >> data[i][0] >> data[i][1];
+  
+  int cnt = 0;
+  FOR(i, 0, n-3) FOR(j, i, n-2) FOR(k, j, n-1) if((data[j][0]-data[i][0])*(data[k][1]-data[i][1])-(data[j][1]-data[i][1])*(data[k][0]-data[i][0])!=0) cnt++;
+  cout << cnt << endl;
+}
