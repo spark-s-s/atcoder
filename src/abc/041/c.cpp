69d09a425c11536baca8d90521b0377d81540c3e
spark <84654021+spark-s-s@users.noreply.github.com>
2023-07-05 12:03:48 +0900

    abc 041 c

---
+++ src/abc/041/c.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+using namespace std;
+using str = string;
+using ll = long long;
+using ld = long double;
+using vb = vector<bool>;
+using vvb = vector<vector<bool>>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vl = vector<long long>;
+using vvl = vector<vector<long long>>;
+#define REP(i, n) for (int i = 0; i < int(n); i++)
+#define ALL(vec) (vec).begin(), (vec).end()
+#define SORT(vec) sort((vec).begin(), (vec).end())
+#define MAX(vec) *max_element((vec).begin(), (vec).end())
+#define MIN(vec) *min_element((vec).begin(), (vec).end())
+#define PB(x) push_back(x);
+#define YES(ans) if (ans) cout << "YES" << endl; else cout << "NO" << endl;
+#define Yes(ans) if (ans) cout << "Yes" << endl; else cout << "No" << endl;
+#define yes(ans) if (ans) cout << "yes" << endl; else cout << "no" << endl;
+const double PI = 3.141592653589793;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<pair<int, int>> a(n);
+  REP(i, n) {
+    cin >> a[i].first;
+    a[i].first *= -1;
+    a[i].second = i + 1;
+  }
+  SORT(a);
+  REP(i, n) cout << a[i].second << endl;
+}
